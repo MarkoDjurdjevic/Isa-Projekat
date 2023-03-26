a8bea4dd976753bdd91c395e2c3e52b564b913e0
AnaElezovic1 <101984815+AnaElezovic1@users.noreply.github.com>
2023-03-26 23:57:29 +0200

    HTML i CSS

---
+++ src/app/update-centre/update-centre.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-update-centre',
+  templateUrl: './update-centre.component.html',
+  styleUrls: ['./update-centre.component.css']
+})
+export class UpdateCentreComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
